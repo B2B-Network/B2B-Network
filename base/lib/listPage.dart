b269c6a85ed7c88a4fdb275d86257267c153bd79
tanushree236 <tanushreemahato600@gmail.com>
2023-12-06 23:13:57 +0530

    commit

--- base/lib/listPage.dart
+++ base/lib/listPage.dart
@@ -1,4 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+// import 'package:image_picker/image_picker.dart';
+
+import 'homePage.dart';
+import 'notification.dart';
+import 'searchPage.dart';
 
 class ListPage extends StatefulWidget {
   const ListPage({super.key});
@@ -10,6 +17,168 @@ class ListPage extends StatefulWidget {
 class _ListPageState extends State<ListPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF0245A3),
+        title: Text("B2B Network"),
+        centerTitle: true,
+      ),
+      body: Align(
+        alignment: Alignment.topLeft,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 34, left: 15),
+                child: Container(
+                  child: Text(
+                    'Add Picture',
+                    style: TextStyle(
+                      fontFamily: 'Mplus1p',
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+
+              // Add the FloatingActionButton
+              Center(
+                child: Container(
+                  width: 350,
+                  height: 160,
+                  padding: EdgeInsets.all(12.0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    color: Color.fromARGB(255, 171, 171, 173),
+                  ),
+                  child: FloatingActionButton(
+                    onPressed: () {
+                      // Handle the onPressed event for posting a picture
+                      // Replace the commented code below with your actual logic for posting a picture.
+                      // _handlePicturePosting();
+                    },
+                    child: Icon(
+                      Icons.add_a_photo,
+                    ),
+                    backgroundColor: Colors.transparent,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 34, left: 15),
+                child: Container(
+                  child: Text(
+                    'Add Caption',
+                    style: TextStyle(
+                      fontFamily: 'Mplus1p',
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 15, right: 15),
+                child: Container(
+                  child: TextFormField(
+                    decoration: InputDecoration(),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Center(
+                child: SizedBox(
+                  width: 150,
+                  height: 45,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xFF0245A3),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      "Upload",
+                      style: TextStyle(
+                        fontFamily: 'Mplus1p',
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Plus',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: 'Notification',
+          ),
+        ],
+        selectedItemColor: Color(0xFF0245A3),
+        unselectedItemColor: Colors.grey,
+        currentIndex: 2,
+        onTap: (int index) {
+          switch (index) {
+            case 0:
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => HomePage(),
+                ),
+              );
+              break;
+            case 1:
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SearchPage(),
+                ),
+              );
+              break;
+            case 2:
+              // Current page, do nothing or handle as needed
+              break;
+            case 3:
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => NotificationPage(),
+                ),
+              );
+              break;
+          }
+        },
+      ),
+    );
   }
+
+  // Replace this method with your actual logic for posting a picture
+  // void _handlePicturePosting() {
+  //   // Implement your logic here
+  // }
 }
